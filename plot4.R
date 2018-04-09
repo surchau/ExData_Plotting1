e4a0727c615405b42f0699d954b9ce069f340fc9
surchau <surchau@users.noreply.github.com>
2018-04-09 13:35:06 -0400

    plot4.png was empty uploading again

--- plot4.R
+++ plot4.R
@@ -27,3 +27,5 @@ with(mydata, {
   legend("topright",col=c("black","red","blue"),lty=1,lwd=2, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
   plot(Global_reactive_power~DateTime, type="l", xlab="datetime", ylab="Global_reactive_power")
   })
+dev.copy(png,file="plot4.png", width=480, height=480)
+dev.off()
